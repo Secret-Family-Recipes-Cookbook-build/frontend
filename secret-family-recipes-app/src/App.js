4377abce937572dbff5a85bbe814bd477698e12b
Jessica Manak <jessica@Jessicas-MacBook-Air.local>
2021-03-30 11:14:27 -0700

    Most of the App and Homepage layout done

--- secret-family-recipes-app/src/App.js
+++ secret-family-recipes-app/src/App.js
@@ -1,11 +1,24 @@
-import logo from './logo.svg';
 import './App.css';
+import { BrowserRouter, Link, Route } from 'react-router-dom';
+import Home from './Home';
+
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        
+        <BrowserRouter>
+          <h1>Secret Family Recipes</h1>
+          <Link to='/'>Home</Link>
+          <Link to='/recipe'></Link>
+          <Link to='/login'>Login Here</Link>
+          <Route exact path='/'>
+            <Home />
+          </Route>
+          <Route path='/recipe'>
+            
+          </Route>
+        </BrowserRouter>
       </header>
     </div>
   );
