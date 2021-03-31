83434641878e92100b1e314f7c8cc21f85e51d3f
Jessica Manak <jessica@Jessicas-MacBook-Air.local>
2021-03-31 09:39:44 -0700

    fixed the layout of my code

--- secret-family-recipes-app/src/App.js
+++ secret-family-recipes-app/src/App.js
@@ -1,30 +1,26 @@
-import React from "react";
-import { BrowserRouter, Link, Route } from 'react-router-dom';
+import React from 'react';
+import { BrowserRouter as Router, Route } from 'react-router-dom';
 import Home from './Home';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-      <BrowserRouter>
-          <Link to='/'>Home</Link>
-          <Link to='./archives'></Link>
-          <Link to='/recipe'></Link>
-          <Link to='/login'>Login Here</Link>
-          <h1>Secret Family Recipes</h1>
-          <Route exact path='/'>
-            <Home />
-          </Route>
-          <Route path='/recipe'>
-            
-          </Route>
-          <Route path='/login'>
+    <Router>
+      <div className='App'>
+            <Route exact path='/'>
+              <Home />
+            </Route>
+            <Route path='/recipe'>
+              
+            </Route>
+            <Route path='/login'>
 
-          </Route>
-        </BrowserRouter>
-      </header>
-    </div>
+            </Route>
+            <Route path='/Search'>
+              
+            </Route>
+      </div>
+    </Router>
   );
 }
 
--- secret-family-recipes-app/src/Home.js
+++ secret-family-recipes-app/src/Home.js
@@ -1,5 +1,17 @@
 import React from 'react';
-import { useHistory } from 'react-router-dom'
+import styled from 'styled-components';
+import { useHistory } from 'react-router-dom';
+
+// Styled Components
+const WrapperDiv = styled.div`
+    display: flex;
+`;
+
+// const Images = styled.div`
+//     display: flex;
+//     background: pink;
+// `;
+
 
 export default function Home() {
     const history = useHistory();
@@ -13,24 +25,21 @@ export default function Home() {
     };
 
     return (
-        <div className='home-app'>
+        <div>
             <div className='top-div'>
             {/* <button onClick={recipeAdd} className='recipe-button'> Add New Recipe </button> */}
             <button onClick={loginForm} className='login-button'> Sign In Here </button>
+            <h1>Secret Family Recipes</h1>
             </div>
-            <h3>Welcome Fam!</h3>        
-            <div className='fam-favs'>
-                <div className='img-one'>
-
-                </div>
-                <div className='img-two'>
-
-                </div>
-                <div className='img-three'>
-
-                </div>
+            <div className='main-pic'>
+            <img src="https://unsplash.it/200/200" alt="delicious meal" />
             </div>
+            <h3>Our Top Three Recipes</h3>        
+            <WrapperDiv>
+                    <img src="https://unsplash.it/200/200" alt="family fav one" />
+                    <img src="https://unsplash.it/200/200" alt='family fav two' />
+                    <img src="https://unsplash.it/200/200" alt='family fav three' />
+            </WrapperDiv>  
         </div>
-
     )
 }
